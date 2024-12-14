30aa0e59e82c3a9a9d7063df90198e006a9b93a3
doanvienuutu <bachvg.22ba13039@usth.edu.vn>
2024-12-14 16:09:41 +0700

    Update labwork1

--- KeepCalmAndDoLabwork1studentmark.py
+++ KeepCalmAndDoLabwork1studentmark.py
@@ -11,8 +11,6 @@
 #    • Show student marks for a given course
 
 
-
-
 #    • Input number of students in a class
 def n0_student(n0_student):
     return n0_student
@@ -28,12 +26,6 @@ def student_in4(id, name, DoB):
     if len(list_student) < n0_student(4):
         list_student.append(student)
 
-student_in4("BA478", "Minh", "12/2/2001")
-student_in4("BI583", "Ngoc", "5/1/2003")
-student_in4("BA163", "An", "9/5/2005")
-student_in4("BI962", "Toan", "2/1/1992")
-list_student = sorted(list_student, key=lambda x: x["Student name"])
-
 #    • Input number of courses
 def n0_course(n0_course):
     return n0_course
@@ -48,48 +40,52 @@ def course_in4(id, name):
     if len(list_course) < n0_course(3):
         list_course.append(course)
 
+#    • Select a course, input marks for student in this course
+def input_mark(course_id, student_id, mark):
+    for course in list_course:
+        if course["Course ID"] == course_id:
+            course["Mark"] = {}
+            if len(student_id) != len(mark):
+                return
+            else:
+                for j in range(len(student_id)):
+                    course["Mark"][student_id[j]] = mark[j]
+
+#    • Show student marks for a given course
+def student_mark(student_id):
+    for student in list_student:
+        if student["Student ID"] == student_id:
+            student["Mark"] = {}
+            for course in list_course:
+                if student_id in course["Mark"]:  
+                    student["Mark"][course["Course name"]] = course["Mark"][student_id]
+
+
+student_in4("BA478", "Cuong", "12/2/2001")
+student_in4("BI583", "Nam", "5/1/2003")
+student_in4("BA163", "An", "9/5/2005")
+student_in4("BI962", "Thuong", "2/1/1992")
+list_student = sorted(list_student, key=lambda x: x["Student name"])
+
 course_in4("MATH1.001", "Calculus 1")
 course_in4("MATH1.002", "Linear algebra")
 course_in4("MATH1.003", "Calculus 2")
 
-#    • Select a course, input marks for student in this course
-def input_mark(i, id, mark):
-    list_course[i]["Mark"] = {}
-    if len(id) != len(mark):
-        return
-    else:
-        for j in range(len(id)):
-            list_course[i]["Mark"].update({id[j]: mark[j]})
+input_mark("MATH1.001", ["BA163", "BA478", "BI962", "BI583"], [15.2, 10.6, 5.1, 17.9])
+input_mark("MATH1.002", ["BA478", "BI962", "BI583", "BA163"], [12.1, 8.5, 18.3, 10.3])
+input_mark("MATH1.003", ["BI583", "BA163", "BA478", "BI962"], [5.3, 3.9, 7.5, 10.1])
 
-input_mark(0, ["BA163", "BA478", "BI962", "BI583"], [15.2, 10.6, 5.1, 17.9])
-input_mark(1, ["BA478", "BI962", "BI583", "BA163"], [12.1, 8.5, 18.3, 10.3])
-input_mark(2, ["BI583", "BA163", "BA478", "BI962"], [5.3, 3.9, 7.5, 10.1])
+student_mark("BA163")
+student_mark("BA478")
+student_mark("BI583")
+student_mark("BI962")
+
+print("---------------------------------------------------------------------------------------------------------------------")
 print("List courses:")
 for i in range(len(list_course)):
     print(list_course[i])
-
-
-#    • Show student marks for a given course
-list_student[0]["Mark"] = {       
-        "MATH1.001 Calculus 1": list_course[0]["Mark"]["BA163"],
-        "MATH1.002 Linear algebra": list_course[1]["Mark"]["BA163"],
-        "MATH1.001 Calculus 2": list_course[2]["Mark"]["BA163"]
-    }
-list_student[1]["Mark"] = {
-        "MATH1.001 Calculus 1": list_course[0]["Mark"]["BA478"],
-        "MATH1.002 Linear algebra": list_course[1]["Mark"]["BA478"],
-        "MATH1.001 Calculus 2": list_course[2]["Mark"]["BA478"]
-    }
-list_student[2]["Mark"] = {
-        "MATH1.001 Calculus 1": list_course[0]["Mark"]["BI583"],
-        "MATH1.002 Linear algebra": list_course[1]["Mark"]["BI583"],
-        "MATH1.001 Calculus 2": list_course[2]["Mark"]["BI583"]
-    }
-list_student[3]["Mark"] = {
-        "MATH1.001 Calculus 1": list_course[0]["Mark"]["BI962"],
-        "MATH1.002 Linear algebra": list_course[1]["Mark"]["BI962"],
-        "MATH1.001 Calculus 2": list_course[2]["Mark"]["BI962"]
-    }
-print("\nList students:")
+print("                        -----------------------------------------------------------------")
+print("List students:")
 for i in range(len(list_student)):
     print(list_student[i])
+print("---------------------------------------------------------------------------------------------------------------------")
