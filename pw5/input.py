c0cf7abb0a73c827bd7f57c6bed920873349001d
doanvienuutu <bachvg.22ba13039@usth.edu.vn>
2024-12-24 23:21:49 +0700

    labwork5

---
+++ pw5/input.py
@@ -0,0 +1,43 @@
+import math, numpy as np
+from domains.Course import Course
+
+list_course = []
+def add_list_course(x):
+    global list_course
+    for i in range(len(x)):
+        list_course.append(x[i])
+    list_course = sorted(list_course, key = lambda x: x.course_id)
+    return list_course
+
+def input_mark(x):
+    current_mark = Course.get_mark(x)
+    for i in range(len(x.list_student_id)):
+        current_mark[x.list_student_id[i]] = math.floor(x.list_student_mark[i]*10) / 10 #• Use math module to round-down student scores to 1-digit decimal upon input, floor()
+    return x.set_mark(current_mark)
+
+list_student = []
+def add_list_student(x):
+    global list_student
+    for i in range(len(x)):
+        list_student.append(x[i])
+    return list_student    
+
+def find_mark(x):
+    global list_course
+    for course in list_course:
+        if x.student_id in course.get_mark(): #get_mark here belong to Course
+            x.get_mark()[course.course_name] = course.get_mark()[x.student_id]
+    return x.set_mark(x.get_mark())
+
+def calculate_gpa(x):
+    global list_course
+    gp = np.array(list(x.get_mark().values()))
+    etcs = np.array([i.etcs for i in list_course])
+    gp_x_etcs = np.multiply(gp, etcs)
+    numerator = np.sum(gp_x_etcs)
+    denominator = np.sum(etcs)
+    x.gpa = math.floor(numerator/denominator*10) / 10
+
+def mark_table(c, s):
+    global list_course, list_student
+    
---
+++ pw5/main.py
@@ -0,0 +1,36 @@
+from domains.Student import Student
+from domains.Course import Course
+import input
+
+cal1 = Course("MATH1.001", "Calculus 1", 4, ["BI034", "BA163", "BA478", "BI962", "BA728", "BI583"], [15.23, 13.98, 10.61, 5.19, 17.93, 18.32])
+chem1 = Course("CHE1.001", "General chemistry 1", 4, ["BI583", "BA163", "BI034", "BA478", "BI962", "BA728"], [5.38, 6.91, 3.96, 7.53, 10.10, 11.32])
+linear = Course("MATH1.002", "Linear algebra", 4, ["BA478", "BI962", "BA728", "BI034", "BI583", "BA163"], [12.19, 13.05, 8.51, 18.33, 13.67, 10.34]) 
+gene = Course("BIO1.002", "Genetics", 3, ["BA728", "BI034", "BA478", "BI583", "BA163", "BI962"], [1.32, 9.24, 9.58, 3.23, 4.26, 1.68])
+bp = Course("ICT1.002", "Basic programming", 4, ["BA163", "BA728", "BA478", "BI583", "BI034", "BI962"], [10.54, 10.54, 4.22, 15.89, 18.85, 9.58])   
+
+list_course = input.add_list_course([cal1, bp, gene, linear, chem1])
+for i in range(len(list_course)):
+    input.input_mark(list_course[i])
+
+f = open("course.txt", "w")
+for i in range(len(list_course)):
+    f.write(str(list_course[i]) + "\n\n")
+f.close()
+
+cuong = Student("BA478", "Nguyen Van Cuong", "12/2/2001")
+nam = Student("BI583", "Tran Van Nam", "5/1/2003")
+an = Student("BA163", "Bat Binh An", "9/5/2005")
+yen = Student("BA728", "Tan Ta Yen", "18/2/2000")
+thuong = Student("BI962", "Dang Tinh Thuong", "2/1/1992")
+alex = Student("BI034", "Alex Hwang", "30/9/1994")
+
+list_student = input.add_list_student([cuong, nam, an, yen, thuong, alex])
+for i in range(len(list_student)):
+    input.find_mark(list_student[i])
+    input.calculate_gpa(list_student[i])
+list_student = sorted(list_student, key=lambda x: x.gpa, reverse = True)
+
+f = open("student.txt", "w")
+for i in list_student:
+    f.write(str(i) + "\n\n")
+f.close()
